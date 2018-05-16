ea0564f9bb1b50151fe5e2477ff49f6d8220667c
vviau <vviau@cubewise.com>
2018-05-16 15:07:47 +1000

    Add accordion to overview

--- overview/plugin.js
+++ overview/plugin.js
@@ -32,16 +32,29 @@ arc.directive("cubewiseInstanceOverview", function () {
                 fromController: 'From Controller'
             };
 
-            $scope.menus = [
+            $scope.options = {
+                tm1ObjectsOpen : true,
+                administrationOpen : true,
+                pluginsOpen : true
+            }
+
+            $scope.tm1Objects = [
                 { key: 'Cubes', description: 'See all cubes',value: '', icon: 'cubes', class: 'object', link:'#/cube/' },
                 { key: 'Dimensions', description: 'See all dimensions',value: '', icon: 'dimensions', class: 'object', link:'#/dimensions/' },
                 { key: 'Processes', description: 'See all Processes',value: '', icon: 'processes', class: 'object', link:'#/processes/' },
-                { key: 'Chores', description: 'See all chores',value: '', icon: 'chores', class: 'object',link:'#/chores/' },
+                { key: 'Chores', description: 'See all chores',value: '', icon: 'chores', class: 'object',link:'#/chores/' }
+                ]
+
+            $scope.configurations = [
                 { key: 'Users', description: 'All TM1 User',value: '', icon: 'fa-user', class: 'admin',link:'' },
                 { key: 'Configuration', description: 'tm1s.cfg settings',value: '', icon: 'configuration', class: 'admin',link:'#/configuration/' },
                 { key: 'Server Logs', description: 'TM1 Server Logs',value: '', icon: 'fa-list-alt', class: 'admin',link:'#/server-logs/' },
-                { key: 'Threads', description: 'All Threads',value: '', icon: 'fa-users', class: 'admin',link:'#/threads/' },
-                { key: 'MDX Query', description: 'Query table with MDX',value: '', icon: 'fa-table', class: 'plugins',link:'#/cubewise-mdx/' }
+                { key: 'Threads', description: 'All Threads',value: '', icon: 'fa-users', class: 'admin',link:'#/threads/' }
+             ]
+
+            $scope.plugins = [
+                { key: 'MDX Query', description: 'Query table with MDX',value: '', icon: 'fa-table', class: 'plugins',link:'#/cubewise-mdx/' },
+                { key: 'Search', description: 'Search Subset',value: '', icon: 'fa-search', class: 'plugins',link:'#/cubewise-arc-search/' }
             ]
 
             $scope.$on("login-reload", function (event, args) {
@@ -51,14 +64,14 @@ arc.directive("cubewiseInstanceOverview", function () {
             // GET USERS COUNT
             $scope.getUsersCount = function () {
                 $http.get(encodeURIComponent($scope.instance) + "/Users/$count").then(function (value) {
-                    $scope.menus[4].value = value.data;
+                    $scope.configurations[0].value = value.data;
                 });
             };
             $scope.getUsersCount();
             // GET CUBE COUNT
             $scope.getCubesCount = function () {
                 $http.get(encodeURIComponent($scope.instance) + "/Cubes/$count").then(function (value) {
-                    $scope.menus[0].value = value.data;
+                    $scope.tm1Objects[0].value = value.data;
                 });
             };
             $scope.getCubesCount();
@@ -66,7 +79,7 @@ arc.directive("cubewiseInstanceOverview", function () {
             // GET DIMENSIONS COUNT
             $scope.getDimensionsCount = function () {
                 $http.get(encodeURIComponent($scope.instance) + "/Dimensions/$count").then(function (value) {
-                    $scope.menus[1].value = value.data;
+                    $scope.tm1Objects[1].value = value.data;
                 });
             };
             $scope.getDimensionsCount();
@@ -74,7 +87,7 @@ arc.directive("cubewiseInstanceOverview", function () {
             // GET PROCESSES COUNT
             $scope.getProcessesCount = function () {
                 $http.get(encodeURIComponent($scope.instance) + "/Processes/$count").then(function (value) {
-                    $scope.menus[2].value = value.data;
+                    $scope.tm1Objects[2].value = value.data;
                 });
             };
             $scope.getProcessesCount();
@@ -82,7 +95,7 @@ arc.directive("cubewiseInstanceOverview", function () {
             // GET CHORES COUNT
             $scope.getChoresCount = function () {
                 $http.get(encodeURIComponent($scope.instance) + "/Chores/$count").then(function (value) {
-                    $scope.menus[3].value = value.data;
+                    $scope.tm1Objects[3].value = value.data;
                 });
             };
             $scope.getChoresCount();
