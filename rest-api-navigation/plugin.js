377e64b5e557fec9d64be6ea5e57adb20e76b5c0
vviau <vviau@cubewise.com>
2018-06-19 10:40:43 +0800

    REST API Navigation - Creation

---
+++ rest-api-navigation/plugin.js
@@ -0,0 +1,66 @@
+
+arc.run(['$rootScope', function ($rootScope) {
+
+    $rootScope.plugin("arcRestApiNavigation", "REST API Navigation", "page", {
+        menu: "tools",
+        icon: "fa-map-o",
+        description: "This plugin can be used to search any TM1 objects",
+        author: "Cubewise",
+        url: "https://github.com/cubewise-code/arc-plugins",
+        version: "1.0.0"
+    });
+
+}]);
+
+arc.directive("arcRestApiNavigation", function () {
+    return {
+        restrict: "EA",
+        replace: true,
+        scope: {
+            instance: "=tm1Instance"
+        },
+        templateUrl: "__/plugins/rest-api-navigation/template.html",
+        link: function ($scope, element, attrs) {
+
+        },
+        controller: ["$scope", "$rootScope", "$http", "$tm1", "$translate", "$timeout", function ($scope, $rootScope, $http, $tm1, $translate, $timeout) {
+
+            //Define variables
+            $scope.defaults = {};
+            $scope.selections = {};
+            $scope.lists = {};
+            $scope.values = {};
+
+            // GET CUBE COUNT
+            $scope.getMetadata = function () {
+                $http.get(encodeURIComponent($scope.instance) + "/$metadata").then(function (result) {
+                    metadataXmlContent = result.data;
+                    //var entityType = result.data.getElementsByTagName("EntityType");
+                    console.log(metadataXmlContent);
+                });
+            };
+            $scope.getMetadata(); 
+
+            //Trigger an event after the login screen
+            $scope.$on("login-reload", function (event, args) {
+
+            });
+
+            //Close the tab
+            $scope.$on("close-tab", function (event, args) {
+                // Event to capture when a user has clicked close on the tab
+                if (args.page == "arcRestApiNavigation" && args.instance == $scope.instance && args.name == null) {
+                    // The page matches this one so close it
+                    $rootScope.close(args.page, { instance: $scope.instance });
+                }
+            });
+
+            //Trigger an event after the plugin closes
+            $scope.$on("$destroy", function (event) {
+
+            });
+
+
+        }]
+    };
+});
