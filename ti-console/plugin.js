e7fd9bc2e385bebec1f3ecbe0b59ba2d8e5b540d
vviau <vviau@cubewise.com>
2018-07-03 17:44:57 +0800

    TI Console - Add alert message

--- ti-console/plugin.js
+++ ti-console/plugin.js
@@ -1,64 +1,84 @@
 
 arc.run(['$rootScope', function ($rootScope) {
 
-    $rootScope.plugin("arcTemplate2", "Ghost TI", "page", {
-        menu: "tools",
-        icon: "fa-snapchat-ghost",
-        description: "This plugin can be used to search any TM1 objects",
-        author: "Cubewise",
-        url: "https://github.com/cubewise-code/arc-plugins",
-        version: "1.0.0"
-    });
+   $rootScope.plugin("arcTemplate2", "Ghost TI", "page", {
+      menu: "tools",
+      icon: "fa-snapchat-ghost",
+      description: "This plugin can be used to search any TM1 objects",
+      author: "Cubewise",
+      url: "https://github.com/cubewise-code/arc-plugins",
+      version: "1.0.0"
+   });
 
 }]);
 
 arc.directive("arcTemplate2", function () {
-    return {
-        restrict: "EA",
-        replace: true,
-        scope: {
-            instance: "=tm1Instance"
-        },
-        templateUrl: "__/plugins/ti-console/template.html",
-        link: function ($scope, element, attrs) {
+   return {
+      restrict: "EA",
+      replace: true,
+      scope: {
+         instance: "=tm1Instance"
+      },
+      templateUrl: "__/plugins/ti-console/template.html",
+      link: function ($scope, element, attrs) {
 
-        },
-        controller: ["$scope", "$rootScope", "$http", "$tm1", "$translate", "$timeout", function ($scope, $rootScope, $http, $tm1, $translate, $timeout) {
+      },
+      controller: ["$scope", "$rootScope", "$http", "$tm1", "$translate", "$timeout", function ($scope, $rootScope, $http, $tm1, $translate, $timeout) {
 
-            //Define variables
-            $scope.defaults = {};
-            $scope.selections = {};
-            $scope.lists = {};
-            $scope.values = {};
+         //Define variables
+         $scope.defaults = {};
+         $scope.selections = {};
+         $scope.lists = {};
+         $scope.values = {};
+         $scope.code = {
+            prolog : '',
+            epilog: ''
+         };
+         $scope.queryStatus = '';
 
-            //Functions
-            $scope.myFirstFunction = function () {
-                console.log("My first function");
+         //Functions
+         $scope.Execute = function () {
+            $scope.queryStatus = 'executing';
+            body = {
+               Process: {
+                  PrologProcedure: $scope.code.prolog,
+                  EpilogProcedure: $scope.code.epilog
+               }
             };
-
-            //Trigger Functions
-            $scope.myFirstFunction();
-
-            //Trigger an event after the login screen
-            $scope.$on("login-reload", function (event, args) {
-
+            var config = {
+               method: "POST",
+               url: encodeURIComponent($scope.instance) + "/ExecuteProcess",
+               data: body
+            };
+            $http(config).then(function (result) {
+               if (result.status == 200 || result.status == 201 || result.status == 204) {
+                  $scope.queryStatus = 'success';
+               } else {
+                  $scope.queryStatus = 'failed';
+               }
             });
+         };
 
-            //Close the tab
-            $scope.$on("close-tab", function (event, args) {
-                // Event to capture when a user has clicked close on the tab
-                if (args.page == "arcTemplate2" && args.instance == $scope.instance && args.name == null) {
-                    // The page matches this one so close it
-                    $rootScope.close(args.page, { instance: $scope.instance });
-                }
-            });
+         //Trigger an event after the login screen
+         $scope.$on("login-reload", function (event, args) {
 
-            //Trigger an event after the plugin closes
-            $scope.$on("$destroy", function (event) {
+         });
 
-            });
+         //Close the tab
+         $scope.$on("close-tab", function (event, args) {
+            // Event to capture when a user has clicked close on the tab
+            if (args.page == "arcTemplate2" && args.instance == $scope.instance && args.name == null) {
+               // The page matches this one so close it
+               $rootScope.close(args.page, { instance: $scope.instance });
+            }
+         });
+
+         //Trigger an event after the plugin closes
+         $scope.$on("$destroy", function (event) {
+
+         });
 
 
-        }]
-    };
+      }]
+   };
 });
