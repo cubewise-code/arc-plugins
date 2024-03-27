2d18248fd84ba154c0a73e0e2268fdaddf15594a
aeremenko <aeremenko@cubewise.com>
2024-03-27 17:15:02 +1100

    Updated copy plugin to use ClipboardJS and get correct names of the selected objects using branch parameter.

--- ls-copy-name/plugin.js
+++ ls-copy-name/plugin.js
@@ -1,98 +1,98 @@
 
 arc.run(['$rootScope', function ($rootScope) {
 
-   $rootScope.plugin("lsCopyCubeName", "Copy Name to clipboard", "menu/cube", {
-      icon: "fa-copy",
-      description: "This plugin copies the selected name into the clipboard.",
-      author: "LS",
-      url: "https://github.com/cubewise-code/arc-plugins",
-      version: "1.0.0"
+   $rootScope.plugin("lsCopyObjectName", "Copy Name to Clipboard", "menu/cube", {
+     icon: "fa-copy",
+     description: "This plugin copies the selected name into the clipboard.",
+     author: "LS",
+     url: "https://github.com/cubewise-code/arc-plugins",
+     version: "1.0.0"
    });
-
-}]);
-arc.run(['$rootScope', function ($rootScope) {
-
-   $rootScope.plugin("lsCopyCubeName", "Copy Name to clipboard", "menu/process", {
-      icon: "fa-copy",
-      description: "This plugin copies the selected name into the clipboard.",
-      author: "LS",
-      url: "https://github.com/cubewise-code/arc-plugins",
-      version: "1.0.0"
+ 
+ }]);
+ arc.run(['$rootScope', function ($rootScope) {
+ 
+   $rootScope.plugin("lsCopyObjectName", "Copy Name to Clipboard", "menu/process", {
+     icon: "fa-copy",
+     description: "This plugin copies the selected name into the clipboard.",
+     author: "LS",
+     url: "https://github.com/cubewise-code/arc-plugins",
+     version: "1.0.0"
    });
-
-}]);
-arc.run(['$rootScope', function ($rootScope) {
-
-   $rootScope.plugin("lsCopyCubeName", "Copy Name to clipboard", "menu/dimension", {
-      icon: "fa-copy",
-      description: "This plugin copies the selected name into the clipboard.",
-      author: "LS",
-      url: "https://github.com/cubewise-code/arc-plugins",
-      version: "1.0.0"
+ 
+ }]);
+ arc.run(['$rootScope', function ($rootScope) {
+ 
+   $rootScope.plugin("lsCopyObjectName", "Copy Name to Clipboard", "menu/dimension", {
+     icon: "fa-copy",
+     description: "This plugin copies the selected name into the clipboard.",
+     author: "LS",
+     url: "https://github.com/cubewise-code/arc-plugins",
+     version: "1.0.0"
    });
-
-}]);
-arc.run(['$rootScope', function ($rootScope) {
-
-   $rootScope.plugin("lsCopyCubeName", "Copy Name to clipboard", "menu/chore", {
-      icon: "fa-copy",
-      description: "This plugin copies the selected name into the clipboard.",
-      author: "LS",
-      url: "https://github.com/cubewise-code/arc-plugins",
-      version: "1.0.0"
+ 
+ }]);
+ arc.run(['$rootScope', function ($rootScope) {
+ 
+   $rootScope.plugin("lsCopyObjectName", "Copy Name to Clipboard", "menu/chore", {
+     icon: "fa-copy",
+     description: "This plugin copies the selected name into the clipboard.",
+     author: "LS",
+     url: "https://github.com/cubewise-code/arc-plugins",
+     version: "1.0.0"
    });
-
-}]);
-arc.run(['$rootScope', function ($rootScope) {
-
-   $rootScope.plugin("lsCopyCubeName", "Copy Name to clipboard", "menu/subsets", {
-      icon: "fa-copy",
-      description: "This plugin copies the selected name into the clipboard.",
-      author: "LS",
-      url: "https://github.com/cubewise-code/arc-plugins",
-      version: "1.0.0"
+ 
+ }]);
+ arc.run(['$rootScope', function ($rootScope) {
+ 
+   $rootScope.plugin("lsCopyObjectName", "Copy Name to Clipboard", "menu/subsets", {
+     icon: "fa-copy",
+     description: "This plugin copies the selected name into the clipboard.",
+     author: "LS",
+     url: "https://github.com/cubewise-code/arc-plugins",
+     version: "1.0.0"
    });
-
-}]);
-arc.run(['$rootScope', function ($rootScope) {
-
-   $rootScope.plugin("lsCopyCubeName", "Copy Name to clipboard", "menu/views", {
-      icon: "fa-copy",
-      description: "This plugin copies the selected name into the clipboard.",
-      author: "LS",
-      url: "https://github.com/cubewise-code/arc-plugins",
-      version: "1.0.0"
+ 
+ }]);
+ arc.run(['$rootScope', function ($rootScope) {
+ 
+   $rootScope.plugin("lsCopyObjectName", "Copy Name to Clipboard", "menu/views", {
+     icon: "fa-copy",
+     description: "This plugin copies the selected name into the clipboard.",
+     author: "LS",
+     url: "https://github.com/cubewise-code/arc-plugins",
+     version: "1.0.0"
    });
-
-}]);
-
-arc.service('lsCopyCubeName', ['$rootScope', '$tm1', 'ngDialog', '$dialogs', '$http', function ($rootScope, $tm1, ngDialog, $dialogs, $http) {
-
-	// The interface you must implement
-	this.execute = function (instance, name) {
-
-		// Create temporary element
-		var el = document.createElement('textarea');
-
-		// Assign the string to copy to the temp element
-		el.value = name;
-
-		// Set element to read only and move out of screen
-		el.setAttribute('readonly', '');
-		el.style = { position: 'absolute', left: '-9999px' };
-
-		// Add element to DOM
-		document.body.appendChild(el);
-
-		// Select text in element
-		el.select();
-
-		// Add selected text to clipboard
-		document.execCommand('copy');
-
-		// Remove temporary element
-		document.body.removeChild(el);
-
-   };
-
-}]);
+ 
+ }]);
+ arc.run(['$rootScope', function ($rootScope) {
+ 
+   $rootScope.plugin("lsCopyObjectName", "Copy Name to Clipboard", "menu/hierarchy", {
+     icon: "fa-copy",
+     description: "This plugin copies the selected name into the clipboard.",
+     author: "LS",
+     url: "https://github.com/cubewise-code/arc-plugins",
+     version: "1.0.0"
+   });
+ 
+ }]);
+ 
+ arc.service('lsCopyObjectName', ['$rootScope', '$tm1', 'ngDialog', '$dialogs', '$http', 'Notification',
+   function ($rootScope, $tm1, ngDialog, $dialogs, $http, Notification) {
+ 
+     this.execute = function (instance, name, branch) {
+       var clipboard = ClipboardJS.copy(branch.label);
+       if (clipboard === branch.label) {
+         Notification.success({
+           title: "<i class='fa share'></i> <b>Copy</b> ",
+           message: "<i class='fa fa-check'></i> Name Copied to Clipboard."
+         });
+       } else {
+         Notification.error({
+           title: "<i class='fa fa-exclamation-triangle'></i> Copy</b>",
+           message: "Unable to copy name to clipboard."
+         });
+       }
+     };
+ 
+   }]);
